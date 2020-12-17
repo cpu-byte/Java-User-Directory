83bb4078bb0cda21beec9d7fda72aa9eb165a22c
Borys Kubisty <dropcmd@gmail.com>
2020-12-17 19:49:03 +0000

    Making Utility Abstract
    - adding abstract keyword to Utility class

--- src/com/company/Utility.java
+++ src/com/company/Utility.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.UUID;
 
-public class Utility {
+public abstract class Utility {
 
     public static String uuidGenerate() {
         return UUID.randomUUID().toString().substring(0, 7);
