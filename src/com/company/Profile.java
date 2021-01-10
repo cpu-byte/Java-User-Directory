7d7bf44e59818d48329e5278c0f7c911107b8c3c
Borys Kubisty <dropcmd@gmail.com>
2021-01-10 14:59:39 +0000

    removal of lastUpdated (redundant)

--- src/com/company/Profile.java
+++ src/com/company/Profile.java
@@ -1,22 +1,13 @@
 package com.company;
 
-import java.util.Date;
-
 public class Profile {
 
     private String biography;
-    private Date lastUpdated;
-
-    private void updated() {
-        this.lastUpdated = new Date();
-    }
-
 
     // standard encapsulation and override methods
 
     public Profile(String biography) {
         this.biography = biography;
-        updated();
     }
 
     public String getBiography() {
@@ -25,22 +16,12 @@ public class Profile {
 
     public void setBiography(String biography) {
         this.biography = biography;
-        updated();
-    }
-
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
     }
 
     @Override
     public String toString() {
         return "Profile{" +
                 "biography='" + biography + '\'' +
-                ", lastUpdated=" + lastUpdated +
                 '}';
     }
 }
