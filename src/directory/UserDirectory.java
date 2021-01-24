af7d5ef655d92285f7a71367b0579667da2c8263
Borys Kubisty <dropcmd@gmail.com>
2021-01-24 17:44:49 +0000

    UserDirectory code comment, ... - removing the try, catch statement, instead returning true or false based on in/valid find

--- src/directory/UserDirectory.java
+++ src/directory/UserDirectory.java
@@ -7,33 +7,45 @@ import directory.elements.user.Profile;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A user directory holds information on all users that are a part of the directory
+ */
 public class UserDirectory implements UserDirectoryInterface {
 
     private List<Employee> users;
 
+    /**
+     * Method to login a user when provided the email and password
+     * @param email     email of an existing user that belong in the user directory
+     * @param password  password of an existing user that belongs in the user directory
+     * @return          upon login: the employee/manager object, invalid credentials will return null value
+     */
     public Employee login(String email, String password) {
         Employee userFound = null;
 
-        try {
+        for (Employee user : users)
+            if (user.getEmail().equals(email) && user.getPassword().equals(password))
+                userFound = user;
 
-            for (Employee user : users)
-                if (user.getEmail().equals(email) && user.getPassword().equals(password))
-                    userFound = user;
+        if (userFound != null) userFound.setOnline();
 
-            if (userFound != null) userFound.setOnline();
-            else throw new Exception("Invalid Credentials");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         return userFound;
     }
 
+    /**
+     * Method to logout a user given the user object
+     * @param user  object of the user (employee/manager) that should be logged out
+     */
     public void logout(Employee user) {
         user.setOffline();
     }
 
+    /**
+     * Method to get the profile of a user given the user ID
+     * @param employeeId    id of the employee/manager that you wish to get the profile of
+     * @return              if found existing ID: the profile of the user, non-found: return null
+     */
     public Profile profileById(String employeeId) {
         Profile foundProfile = null;
 
@@ -44,30 +56,57 @@ public class UserDirectory implements UserDirectoryInterface {
         return foundProfile;
     }
 
+    /**
+     * Update the Profile biography of a user as a manager
+     * @param author    authorised author, required to be a instance of manager
+     * @param target    the user who will have the new biography
+     * @param newBio    new biography that will be applied to the Profile of the user
+     * @return          true: successfully updated, false: invalid permission (author not authorised)
+     */
     public boolean updateUserBio(Employee author, Employee target, String newBio) {
         if (author instanceof Manager) target.setProfile(new Profile(newBio));
         else return false;
         return true;
     }
 
+    /**
+     * Get the number of users contains in the user directory
+     * @return  value of users in the user directory
+     */
     public int numOfUsers() {
         return this.users.size();
     }
 
+
     // standard encapsulation and override methods
 
+    /**
+     * Constructor for empty UserDirectory object instance
+     */
     public UserDirectory() {
         this.users = new ArrayList<>();
     }
 
+    /**
+     * Constructor for a populated UserDirectory object instance
+     * @param users populated list of users for the UserDirectory
+     */
     public UserDirectory(List<Employee> users) {
         this.users = users;
     }
 
+    /**
+     * Getter method for the UserDirectory's users field
+     * @return  current list of users
+     */
     public List<Employee> getUsers() {
         return users;
     }
 
+    /**
+     * Setter method for the UserDirectory's users field
+     * @param users new list of users
+     */
     public void setUsers(List<Employee> users) {
         this.users = users;
     }
