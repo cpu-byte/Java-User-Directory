956ff15ea4c8e64c4e525584ccbcfb6f85cafdd3
Borys Kubisty <dropcmd@gmail.com>
2021-01-24 17:55:01 +0000

    intellij generated java doc

---
+++ src/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"directory"},{"l":"directory.elements"},{"l":"directory.elements.user"},{"l":"pattern.command"}];updateSearchResults();
